aee6613a8bfc9b1a6f664cbb0fa31f31d501a773
TAKEUCHI Daisuke <takenoco82@gmail.com>
2017-05-21 00:34:15 +0900

    インストール用のスクリプトを新規作成した。

---
+++ Modules/Excel/install.ps1
@@ -0,0 +1,11 @@
+﻿$ScriptDir = Split-Path $MyInvocation.MyCommand.Path
+$UserModuleDir = $env:PSModulePath -split ";" | ?{ $_ -like "${env:USERPROFILE}*" }
+$CurrentModuleDir = Join-Path $UserModuleDir (Get-Item $ScriptDir).BaseName
+
+$mklinkArgs = @()
+$mklinkArgs += [PSCustomObject]@{"source"="$ScriptDir"; "target"="$CurrentModuleDir"}
+
+$Command = "/c cd /d `"$ScriptDir`""
+$mklinkArgs | %{ $Command += " `& mklink /d `"$($_.target)`" `"$($_.source)`"" }
+
+Start-Process cmd -Verb Runas -WindowStyle Hidden -ArgumentList $Command
